63a591c8b40d3ec431fd3b2f73688e6b4724dfc2
Andrew Sanchez <druu831@gmail.com>
2023-08-01 15:46:40 -0700

    dashboard and login routes/controller

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static('./public'));
 app.use(express.json());
 
 app.use(notFoundMiddleware);
